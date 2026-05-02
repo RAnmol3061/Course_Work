a99c44b03ea42fd2ef88f1eb4617192b5f57964b
R_Anmol <anmolmain3061@gmail.com>
2026-05-02 15:56:11 +0530

    refactor: Replace hardcoded variable with class
    - Use Vector normalization instead of finding force components manually
    - Increased trail size

--- 1st_Year/Semester-II/Practical-29.py
+++ 1st_Year/Semester-II/Practical-29.py
@@ -10,24 +10,27 @@ running = True
 dt = 0.001
 
 class celestial_bodies:
-    def __init__(self, name, mass, velocity) -> None:
+    def __init__(self, name, mass, position, velocity) -> None:
         self.name = name
         self.mass = mass
+        self.position = position
         self.velocity = velocity
+        self.acceleration = pygame.Vector2()
+        self.trail = deque(maxlen=500)
 
-sun = celestial_bodies('sun', 10000, pygame.Vector2())
-earth 
+screen_middle = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+sun = celestial_bodies('sun', 10000, screen_middle, pygame.Vector2(0,0))
+earth = celestial_bodies('earth', 1, pygame.Vector2(440,160), pygame.Vector2(0,500))
+moon = celestial_bodies('moon', 0.5, pygame.Vector2(), pygame.Vector2(0,0))
         
+G = 10000
+M_ES = G * sun.mass * earth.mass # Numerator of F = GMeMs/r^2 where Me mass of Earth and Ms is mass of sun
 
-sun = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-#Normalized Constant
-G, M1, M2 = 10000, 10000, 1
-
-object = pygame.Vector2(440,160)    # Object and velocity are the only two values we can set
-velocity = pygame.Vector2(0, 500)
-object_pos = deque(maxlen=100)
-object_pos.append(object)
+#object = pygame.Vector2(440,160)    # Object and velocity are the only two values we can set
+#velocity = pygame.Vector2(0, 500)
+#object_pos = deque(maxlen=100)
+#object_pos.append(object)
 
 while running:
     # poll for events
@@ -37,27 +40,26 @@ while running:
             running = False
 
     screen.fill("black")
-    pygame.draw.circle(screen, "red", sun, 40)
-    pygame.draw.circle(screen,"blue",object, 20)
+    pygame.draw.circle(screen, "red", sun.position, 40)
+    pygame.draw.circle(screen,"blue",earth.position, 20)
 
-    for pos in object_pos:
-        pygame.draw.circle(screen, "white", pos, 2)
+    if len(earth.trail) >= 2:
+        pygame.draw.aalines(screen, "white", False, earth.trail)
 
-    dx = sun.x - object.x 
-    dy = sun.y - object.y
-     
-    distance = pygame.math.Vector2.distance_to(sun, object) #r
+    displacement = sun.position - earth.position 
+    displacement_unit = displacement.normalize()
 
-    force = (G*M1*M2)/(distance * distance)       
-    fx, fy = (force * (dx/distance)), (force * (dy/distance))               
-    force_vector = pygame.Vector2(fx, fy)
+    distance = pygame.math.Vector2.distance_squared_to(sun.position, earth.position) 
+
+    force = M_ES/distance       
+    force_vector = force * displacement_unit
                                   
-    acceleration = force_vector / M2                      #Using Semi-Euler method
-    velocity = velocity + (acceleration * dt)
-    object = object + (velocity * dt)
+    earth.acceleration = force_vector / earth.mass               #Using Semi-Euler method
+    earth.velocity = earth.velocity + (earth.acceleration * dt)
+    earth.position = earth.position + (earth.velocity * dt)
 
 
-    object_pos.append(object)
+    earth.trail.append(earth.position)
     pygame.display.flip()
     clock.tick(60)
 
