46d77b92c0870f7aece8f35ebdc4fc9ecdb2a754
R_Anmol <anmolmain3061@gmail.com>
2026-07-15 18:46:29 +0530

    feat: complete Practical-11

---
+++ 1st_Year/Semester-II/Practical-11.py
@@ -0,0 +1,23 @@
+exp_list = [10, 11, 12, 13, 14, 15]
+
+# Insert
+exp_list.insert(2, 100)
+print(exp_list)
+
+# Remove
+exp_list.remove(100)
+print(exp_list)
+
+# Append
+exp_list.append(100)
+print(exp_list)
+
+# Length
+print(len(exp_list))
+
+# Pop
+print(f"Element Removed: {exp_list.pop()}")
+
+# Clear
+exp_list.clear()
+print(exp_list)
