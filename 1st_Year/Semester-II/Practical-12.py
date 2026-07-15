0f91ad544b63b8656cfda192689e04c3e018d98e
R_Anmol <anmolmain3061@gmail.com>
2026-07-15 18:55:42 +0530

    feat: complete Practical-12

---
+++ 1st_Year/Semester-II/Practical-12.py
@@ -0,0 +1,18 @@
+# Find the lenght of a list
+exp_list = [10, 20, 30]
+
+# Using Built-In len()
+print(
+    len(exp_list)
+)  # Time Complexity: O(1), Reason: There is a size variable in the header of every list in python. Magic of OOPs
+
+
+# Using User-Defined Fucntion
+def get_length(sequence: list[int]) -> int:
+    c = 0
+    for i in sequence:
+        c += 1
+    return c
+
+
+print(get_length(exp_list))
