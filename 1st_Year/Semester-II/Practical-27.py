fe2f03f1937ee22d54a0d349614f79016fc2b8dd
R_Anmol <anmolmain3061@gmail.com>
2026-07-15 22:37:58 +0530

    feat: complete Practical-27

---
+++ 1st_Year/Semester-II/Practical-27.py
@@ -0,0 +1,17 @@
+exp_dict = {"one": 1, "two": 2}  # Example Dictionary
+
+# Change
+exp_dict["one"] = 7
+print(exp_dict)
+
+# Remove
+del exp_dict["one"]
+print(exp_dict)
+
+# Add
+exp_dict["three"] = 3
+print(exp_dict)
+
+# Delete
+exp_dict.clear()
+print(exp_dict)
